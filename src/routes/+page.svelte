c90b4520e9e85ce7b6699da0bc636bdea8482d8d
shahank42 <shounakghosh42@gmail.com>
2024-04-13 00:35:57 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Button from "$lib/components/ui/button/button.svelte";
+
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button>Clicky</Button>
