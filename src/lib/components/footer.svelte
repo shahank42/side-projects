589445ef4f83b26536bee32110e2075e50404871
shahank42 <shounakghosh42@gmail.com>
2024-04-21 10:55:57 +0530

    fix footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
 	import { Separator } from '$lib/components/ui/separator';
+	import MaxWidthWrapper from './max-width-wrapper.svelte';
 </script>
 
-<footer class="flex w-full justify-center border-t border-input py-5">
-	<span class="flex gap-3 text-muted-foreground">
-		<span class="">
+<footer class="border-t border-input py-5">
+	<MaxWidthWrapper class="flex w-full justify-center">
+		<span class="text-muted-foreground">
 			Created by <a href="https://github.com/shahank42">shahank42</a> for the
 			<a href="https://supabase.com/blog/supabase-oss-hackathon">
 				Supabase Open Source Hackathon 2024
 			</a>
 		</span>
-	</span>
+	</MaxWidthWrapper>
 </footer>
